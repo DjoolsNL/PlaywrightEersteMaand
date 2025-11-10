1695b161f1046dda2f1f31c0490081ca9463453d
jules vannuijs <jules.vannuijs@bee-organisation.com>
2025-11-10 19:58:43 +0100

    details

--- BeeEersteMaand/tests/K05_de_DOM_Heerlen.spec.ts
+++ BeeEersteMaand/tests/K05_de_DOM_Heerlen.spec.ts
@@ -4,28 +4,6 @@ test.describe('Oefenen met de DOM', () => {
 
     test('1. Gemeente Heerlen', async ({page}) => {
         await page.goto('https://www.heerlen.nl');
-
-        await page.getByRole('button', { name: 'Accepteer alle cookies' }).click();
-        await page.getByRole('link', { name: 'Verhuizen' }).click();
-        await page.getByRole('link', { name: 'Bouwen en wonen' }).click();await page.getByRole('link', { name: 'Huisnummering' }).click();
-        await page.getByRole('link', { name: 'Regel bij Gegevenshuis(' }).click();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-     
+   
     })
 })
