855e28677c0343656cd49b9a2f92c0f20b876ae8
jules vannuijs <jules.vannuijs@bee-organisation.com>
2025-11-10 20:18:16 +0100

    details

--- BeeEersteMaand/tests/K05_de_DOM_Heerlen.spec.ts
+++ BeeEersteMaand/tests/K05_de_DOM_Heerlen.spec.ts
@@ -3,7 +3,7 @@ import { test } from '@playwright/test';
 test.describe('Oefenen met de DOM', () => {
 
     test('1. Gemeente Heerlen', async ({page}) => {
-        await page.goto('https://www.heerlen.nl');
+        await page.goto('https://www.cbs.nl');
    
     })
 })
