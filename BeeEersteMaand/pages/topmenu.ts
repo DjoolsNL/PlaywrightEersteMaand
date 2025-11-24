1c470acb66edde19ca4c236ab19f9ad973d057dc
jules vannuijs <jules.vannuijs@bee-organisation.com>
2025-11-24 20:51:41 +0100

    Lezen en Opdrachten opgeschoond

---
+++ BeeEersteMaand/pages/topmenu.ts
@@ -0,0 +1,23 @@
+import { test, expect, request, Page, Locator } from '@playwright/test';
+
+export class Topmenu {
+   page: Page;
+   home: Locator;
+   leerbedrijf: Locator;
+   educatief: Locator;
+   baanperspectief: Locator;
+   portfolio: Locator;
+   templates: Locator;
+
+   constructor(page: Page){
+      this.page = page;
+      this.home = page.getByRole('link', { name: 'Home' });
+      this.leerbedrijf = page.getByRole('link', { name: 'Leerbedrijf' });
+      this.educatief = page.getByRole('link', { name: 'Educatief' });
+      this.baanperspectief = page.getByRole('link', { name: 'Baanperspectief' });
+      this.portfolio = page.getByRole('link', { name: 'Portfolio' });
+      this.templates = page.getByRole('link', { name: 'Templates', exact: true });
+   }
+
+
+}
--- BeeEersteMaand/tests/gotoPage.spec.ts
+++ BeeEersteMaand/tests/gotoPage.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect, request } from '@playwright/test';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import pages from '../pageNames.json';
+import fileNames from '../htmlfile_names.json';
+import {Topmenu} from '../pages/topmenu';
+test.use({ viewport: { width: 1920, height: 1080 } });
+
+// schrijf je eigen toelichtingen bij de coderegels
 
 test( 'Go to any page', async ( { page } ) => {
+   const topmenu = new Topmenu(page)
+   await goTo( fileNames.istqb );
+   // await goTo( fileNames.index );
+   // await goTo( fileNames.subreddit );
+
    async function goTo ( fileName: string ) {
       let filePath = path.resolve( `test website/html/${fileName}.html` );
       const fileUrl = `file://${filePath}`;
@@ -11,8 +20,27 @@ test( 'Go to any page', async ( { page } ) => {
       await page.goto( fileUrl );
    }
 
-   // use variable pages to pick the page you want to go to
-   await goTo( pages.istqb );
-   await goTo( pages.index );
-   await goTo( pages.subreddit );
+   // // class Topmenu in actie
+   await topmenu.educatief.click( { delay: 1000 } );
+   await topmenu.templates.click( { delay: 1000 } );
+   await topmenu.leerbedrijf.click( { delay: 1000 } );
+   await topmenu.portfolio.click( { delay: 1000 } ); 
+   await topmenu.baanperspectief.click( { delay: 1000 } );
+   await topmenu.home.click( { delay: 1000 } );
+   
+   // // en dezelfde reeks clicks zonder de class
+   await page.getByRole('link', { name: 'Educatief' }).click( { delay: 1000 } );
+   await page.getByRole('link', { name: 'Templates', exact: true }).click( { delay: 1000 } );
+   await page.getByRole('link', { name: 'Leerbedrijf' }).click( { delay: 1000 } );
+   await page.getByRole('link', { name: 'Portfolio' }).click( { delay: 1000 } );
+   await page.getByRole('link', { name: 'Baanperspectief' }).click( { delay: 1000 } );
+   await page.getByRole('link', { name: 'Home' }).click( { delay: 1000 } );
+   
+   // // class Topmenu in actie
+   await topmenu.educatief.click( { delay: 1000 } );
+   await topmenu.templates.click( { delay: 1000 } );
+   await topmenu.leerbedrijf.click( { delay: 1000 } );
+   await topmenu.portfolio.click( { delay: 1000 } ); 
+   await topmenu.baanperspectief.click( { delay: 1000 } );
+   await topmenu.home.click( { delay: 1000 } );
 } )
