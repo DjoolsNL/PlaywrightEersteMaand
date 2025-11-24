fbbc5015a7dce9fe1fed786e03846ef587996b5a
jules vannuijs <jules.vannuijs@bee-organisation.com>
2025-11-24 17:24:29 +0100

    scaling embedded content fixed

--- BeeEersteMaand/tests/htmlvalidator.spec.ts
+++ BeeEersteMaand/tests/htmlvalidator.spec.ts
@@ -1,11 +1,38 @@
 import { test, expect, request } from '@playwright/test';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import pages from '../pageNames.json';
 
-test( 'Check all links ', async ( { page } ) => {
-   await page.goto( "https://validator.w3.org/" );
+test( 'Validate html', async ( { page } ) => {
+   async function goTo ( fileName: string ) {
+      let filePath = path.resolve( `test website/html/${fileName}.html` );
+      const fileUrl = `file://${filePath}`;
 
-   await page.getByRole( 'link', { name: 'Validate by Direct Input' } ).click();
-   await page.getByLabel( 'Enter the Markup to validate' ).click();
+      await page.goto( fileUrl );
+      const htmlContent = await page.content();
+
+      // Send the HTML content to the W3C validator API via a POST request
+      const response = await page.request.post( 'https://validator.w3.org/nu/', {
+         headers: {
+            'Content-Type': 'text/html; charset=utf-8',
+         },
+         data: htmlContent
+      } );
+
+      // Check that the API request was successful (status 200)
+      expect( response.ok() ).toBeTruthy();
+
+      // html komt als buffer
+      const jsonResponse = await response.body();
+
+      console.log( jsonResponse.toString() );
+   }
+
+   await goTo(pages.index);
+
+
+
+
+
 
 } )
